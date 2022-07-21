ffedbeb0a2d3db7d4b96f7ddd33a6cc688c8292e
Jakub Kowalski <contact@jakubkowalski.dev>
2022-07-21 19:57:19 +0200

    🥷Feat: ResetCSS and GlobalStyles to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
+import ResetCss from "../src/Global/styles/ResetCss";
+import GlobalStyles from "../src/Global/styles/GlobalStyles";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ResetCss />
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App;
